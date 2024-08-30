8e76610b428658c238ab612da7dce3beb0791982
Moy0101 <119554995+Moy0101@users.noreply.github.com>
2024-08-30 10:44:34 -0600

    matriz ejercicio

---
+++ matrix.py
@@ -0,0 +1,15 @@
+numerofavorito=[["Juan",5],["Mary",2],["Pepe",17]]
+for row in numerofavorito:
+    print(row)
+
+artistas=[["Nombre real", "Nombre artístico"],
+["María Guadalupe Araujo Yong", "Ana Gabriel"],
+["Alberto Aguilera Valadez", "Juan Gabriel"],
+["Hassan Emilio Kabande Laija", "Peso Pluma"],
+["Juan Luis Londoño Arias", "Maluma"],
+["Elmer Figueroa Arce", "Chayanne"],
+["Adele Laurie Blue Adkins", "Adele"],
+["Carolina Giraldo Navarro", "Karol G"],
+["Rosalía Vila Tobella", "Rosalía"]]
+for ro in artistas:
+    print(ro)
