40721b52094a5668d6b818cd95ec395eb2fecdf7
Moy0101 <119554995+Moy0101@users.noreply.github.com>
2024-09-02 21:33:50 -0600

    clases de python basico (ejercios)

--- matrix.py
+++ matrix.py
@@ -1,15 +1,9 @@
-numerofavorito=[["Juan",5],["Mary",2],["Pepe",17]]
-for row in numerofavorito:
-    print(row)
+def calular_promedio(numeros):
+    suma = sum (numeros)
+    cantidad = len(numeros)
+    promedio = suma / cantidad
+    print("El promedio es:", promedio)
 
-artistas=[["Nombre real", "Nombre artístico"],
-["María Guadalupe Araujo Yong", "Ana Gabriel"],
-["Alberto Aguilera Valadez", "Juan Gabriel"],
-["Hassan Emilio Kabande Laija", "Peso Pluma"],
-["Juan Luis Londoño Arias", "Maluma"],
-["Elmer Figueroa Arce", "Chayanne"],
-["Adele Laurie Blue Adkins", "Adele"],
-["Carolina Giraldo Navarro", "Karol G"],
-["Rosalía Vila Tobella", "Rosalía"]]
-for ro in artistas:
-    print(ro)
+valor ={1,2,3,4,5}
+
+calular_promedio (valor)
